89aa7aad7e37fb6ff86646fdd2b4120064def1e1
Henrique Stroher <henriquestroher2003@gmail.com>
2025-06-16 15:03:56 -0300

    Finalizado portfolio, possiveis futuras alterações

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.buttons')
+
+buttons.forEach(button => button.addEventListener('click', () => clickedButton(button)));
+
+const clickedButton = (button) => {
+    const containers = document.querySelectorAll('.container');
+
+    containers.forEach(container => container.classList.remove('show'));
+
+    const hrefButton = button.getAttribute('href').substring(1);
+    const selectId = document.getElementById(hrefButton);
+
+    if (hrefButton === 'home') {
+        selectId.classList.add('show');
+    } else { 
+        const portfolio = document.getElementById('portfolio');
+        portfolio.classList.add('show');
+        selectId.classList.add('show')
+    }
+}
+
